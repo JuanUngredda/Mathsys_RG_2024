d09efe89e1dabc8fc47bcf1dab1441000d7667f6
jungredda <ungredda@esteco.com>
2024-03-20 12:53:51 +0100

    first commit

--- Launcher.py
+++ Launcher.py
@@ -1,5 +1,3 @@
-import warnings
-
 import torch
 from botorch.test_functions import Branin
 
@@ -15,7 +13,7 @@ if __name__ == "__main__":
     black_box_function = Branin(noise_std=1e-6, negate=True)
     model = GPModelWrapper()
     loop = OptimizationLoop(black_box_func=black_box_function,
-                            ei_type=EIType.BOTORCH_EXPECTED_IMPROVEMENT,
+                            ei_type=EIType.BOTORCH_MC_EXPECTED_IMPROVEMENT,
                             bounds=bounds,
                             model=model,
                             seed=0,
--- bo/acquisition_functions/acquisition_functions.py
+++ bo/acquisition_functions/acquisition_functions.py
@@ -1,11 +1,18 @@
 from enum import Enum, auto
+from typing import Optional, Union, Any
 
-from botorch.acquisition import AnalyticAcquisitionFunction, MCAcquisitionFunction, ExpectedImprovement
+import torch
+from botorch.acquisition import ExpectedImprovement, \
+    qExpectedImprovement, MCAcquisitionObjective
+from botorch.acquisition.objective import PosteriorTransform
+from botorch.models.model import Model
+from botorch.sampling import MCSampler, SobolQMCNormalSampler
 from torch import Tensor
 
 
 class EIType(Enum):
     BOTORCH_EXPECTED_IMPROVEMENT = auto()
+    BOTORCH_MC_EXPECTED_IMPROVEMENT = auto()
     MATHSYS_EXPECTED_IMPROVEMENT = auto()
     MATHSYS_MC_EXPECTED_IMPROVEMENT = auto()
 
@@ -13,17 +20,54 @@ class EIType(Enum):
 def ExpectedImprovementFactory(ei_type, model, best_value):
     if ei_type is EIType.BOTORCH_EXPECTED_IMPROVEMENT:
         return ExpectedImprovement(model=model, best_f=best_value)
+    elif ei_type is EIType.BOTORCH_MC_EXPECTED_IMPROVEMENT:
+        qmc_sampler = SobolQMCNormalSampler(sample_shape=torch.Size([1000]))
+        return qExpectedImprovement(model=model, best_f=best_value, sampler=qmc_sampler)
     elif ei_type is EIType.MATHSYS_EXPECTED_IMPROVEMENT:
-        pass
+        return MathsysExpectedImprovement(model=model, best_f=best_value)
     elif ei_type is EIType.MATHSYS_MC_EXPECTED_IMPROVEMENT:
+        return MathsysMCExpectedImprovement(model=model, best_f=best_value)
+
+
+class MathsysExpectedImprovement(ExpectedImprovement):
+
+    def __init__(self, model: Model, best_f: Union[float, Tensor],
+                 posterior_transform: Optional[PosteriorTransform] = None, maximize: bool = True, **kwargs):
+        super().__init__(model, best_f, posterior_transform, maximize, **kwargs)
+
+    def forward(self, X: Tensor) -> Tensor:
+        r"""Evaluate Expected Improvement on the candidate set X.
+
+        Args:
+            X: A `(b1 x ... bk) x 1 x d`-dim batched tensor of `d`-dim design points.
+                Expected Improvement is computed for each point individually,
+                i.e., what is considered are the marginal posteriors, not the
+                joint.
+
+        Returns:
+            A `(b1 x ... bk)`-dim tensor of Expected Improvement values at the
+            given design points `X`.
+        """
         pass
 
 
-class MathsysExpectedImprovement(AnalyticAcquisitionFunction):
-    def forward(self, X: Tensor) -> Tensor:
-        pass
+class MathsysMCExpectedImprovement(qExpectedImprovement):
+    def __init__(self, model: Model, best_f: Union[float, Tensor], sampler: Optional[MCSampler] = None,
+                 objective: Optional[MCAcquisitionObjective] = None,
+                 posterior_transform: Optional[PosteriorTransform] = None, X_pending: Optional[Tensor] = None,
+                 **kwargs: Any) -> None:
+        super().__init__(model, best_f, sampler, objective, posterior_transform, X_pending, **kwargs)
 
-
-class MathsysMCExpectedImprovement(MCAcquisitionFunction):
     def forward(self, X: Tensor) -> Tensor:
+        r"""Evaluate qExpectedImprovement on the candidate set `X`.
+
+        Args:
+            X: A `batch_shape x q x d`-dim Tensor of t-batches with `q` `d`-dim design
+                points each.
+
+        Returns:
+            A `batch_shape'`-dim Tensor of Expected Improvement values at the given
+            design points `X`, where `batch_shape'` is the broadcasted batch shape of
+            model and input `X`.
+        """
         pass
